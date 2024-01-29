d0e5fdf9f9e46eb649ab7e0ed606adfac1037f42
DaymensCode <Daymenscode@gmail.com>
2024-01-29 09:41:10 -0500

    added unique:false to the tag and product references

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -14,11 +14,14 @@ router.get('/', async (req, res) => {
 
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 
-
 });
+
+
+
+
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
@@ -44,7 +47,7 @@ router.post('/', async (req, res) => {
     res.status(200).json(newTag);
   } catch (err) {
     
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
