310c892cac753e4a7ede049acff9fc43896103c4
DaymensCode <Daymenscode@gmail.com>
2024-01-29 13:33:25 -0500

    added all routes in category-routes and tested them successfully in HTTPie

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,23 +59,55 @@ router.get('/:id', async (req, res) => {
      });
       return;
     }
-    res.status(200).json(tagData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(200).json(newCategory);
+  } catch (err) {
+    
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No tag with that ID' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
