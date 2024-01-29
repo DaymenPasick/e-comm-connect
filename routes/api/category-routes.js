d451a469ead6730d74e98cdac16367389157cb9b
DaymensCode <Daymenscode@gmail.com>
2024-01-29 13:22:59 -0500

    similar to above commit, created get route for single category request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
       {
        model: Product,
 
-       //will hide tag id from req response
+       //will hide product id from req response
        attributes: {
         exclude: ['id']
        }
@@ -19,15 +19,50 @@ router.get('/', async (req, res) => {
       }
     ],
   
-    //will hide tag id from req response  
+    //will hide category id from req response  
     attributes: {
       exclude: ['id']
     },
-  // be sure to include its associated Products
+  
+  });
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {include:
+       [
+        {
+         model: Product,
+
+         //will hide product id from req response
+         attributes: {
+          exclude: ['id']
+         }
+         
+        }
+      ],
+
+      //will categorytag id from req response
+      attributes: {
+        exclude: ['id']
+      },
+    
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No tag with this id!',
+     });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
