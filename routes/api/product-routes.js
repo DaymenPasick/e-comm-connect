f23c432a7e31ea1af82f6868b5eca94aabc5c0a3
DaymensCode <Daymenscode@gmail.com>
2024-01-29 12:15:41 -0500

    routes for get all products and get product by id are populated and generating proper responses from HTTPie

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -43,15 +43,25 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   try {
-    const tagData = await Tag.findByPk(req.params.id, {include:
-       [{
-         model: Product,
+    const productData = await Product.findByPk(req.params.id, {include:
+       [
+        {
+         model: Category,
 
          //will hide tag id from req response
          attributes: {
           exclude: ['id']
          }
-        }],
+        },
+        {
+         model: Tag,
+
+         //will hide tag id from req response
+         attributes: {
+          exclude: ['id']
+         }
+        }
+      ],
 
       //will hide tag id from req response
       attributes: {
@@ -59,12 +69,12 @@ router.get('/:id', async (req, res) => {
       },
     
     });
-    if (!tagData) {
+    if (!productData) {
       res.status(404).json({ message: 'No tag with this id!',
      });
       return;
     }
-    res.status(200).json(tagData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
