3ad8f4b981cd2df34a6a6e4461d239b3f70714ba
DaymensCode <Daymenscode@gmail.com>
2024-01-29 11:35:57 -0500

    added attributes to exclude the product id in get all tags and get single tag requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,12 @@ router.get('/', async (req, res) => {
 
 
   try {
-    const tagData = await Tag.findAll({include: [{ model: Product }]});
+    const tagData = await Tag.findAll({include: [{
+       model: Product,
+       attributes: {
+        exclude: ['id']
+       }
+    }]});
 
     res.status(200).json(tagData);
   } catch (err) {
@@ -24,8 +29,13 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Tag.findByPk(req.params.id,
-      {include: [{ model: Product }]} );
+    const tagData = await Tag.findByPk(req.params.id, {include: [{
+         model: Product,
+         attributes: {
+          exclude: ['id']
+         }
+      
+      }]});
     if (!tagData) {
       res.status(404).json({ message: 'No tag with this id!',
      });
